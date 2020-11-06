316b6858b62dddb8b566284925d8c8b5b8f2f4f8
Casey Evanoff <casey@scribd.com>
2020-11-06 15:26:50 -0700

    add new flag cache-build-files

--- dist/index.js
+++ dist/index.js
@@ -47,10 +47,11 @@ const execa = __webpack_require__(5447);
  * or the Xcode version changes.
  */
 
-const generateCacheKey = async () => {
+const generateCacheKey = async (configuration, skippingSchemes, xcconfig, noUseBinaries) => {
     const resolved = fs.readFileSync("Cartfile.resolved");
+    const xcconfigContents = fs.existsSync(xcconfig) ? fs.readFileSync(xcconfig) : '';
     const {stdout} = await execa('xcodebuild', ['-version']);
-    const hash = crypto.createHash('sha1').update(resolved+stdout).digest('hex');
+    const hash = crypto.createHash('sha1').update(resolved+stdout+configuration+xcconfigContents+skippingSchemes+noUseBinaries).digest('hex');
     return `devbotsxyz:carthage:${process.env.GITHUB_REPOSITORY}:${hash}`;
 };
 
@@ -59,10 +60,11 @@ const generateCacheKey = async () => {
  * Restore the cache.
  */
 
-const restoreCache = async ({cacheKey}) => {
-    const key = cacheKey || await generateCacheKey();
+const restoreCache = async (options) => {
+    const key = options.cacheKey || await generateCacheKey(options);
+    const restoreKeys = options.cacheBuildFiles ? [generateBuildFilesCacheKey()] : null;
     core.info(`Restoring cache with key ${key}`);
-    const result = await cache.restoreCache(["Carthage"], key);
+    const result = await cache.restoreCache(["Carthage"], key, restoreKeys);
     core.info(`Result of cache.restore is ${result !== undefined}`);
     return result !== undefined;
 };
@@ -72,11 +74,12 @@ const restoreCache = async ({cacheKey}) => {
  * Save the cache.
  */
 
-const saveCache = async ({cacheKey}) => {
+const saveCache = async (options) => {
     try {
-        const key = cacheKey || await generateCacheKey();
+        const key = options.cacheKey || await generateCacheKey(options);
+        const restoreKeys = options.cacheBuildFiles ? [generateBuildFilesCacheKey()] : null;
         core.info(`Saving cache with key ${key}`);
-        await cache.saveCache(["Carthage"], key);
+        await cache.saveCache(["Carthage"], key, restoreKeys);
         return true;
     } catch (error) {
         core.error(`Carthage could not be cached: ${error.message}`);
@@ -85,29 +88,117 @@ const saveCache = async ({cacheKey}) => {
 };
 
 
-const parseConfiguration = () => {
-    const configuration = {
-        verbose: core.getInput("verbose") === "true",
-        noUseBinaries: core.getInput("no-use-binaries") === "true",
-        platform: core.getInput("platform"),
-        cache: core.getInput("cache") === "true", // TODO cache=true should be a default
-        cacheKey: core.getInput("cache-key"),
-        gitHubToken: core.getInput("github-token", {required: true}), // Not required when using SSH?
-    };
-    return configuration;
+/**
+ * Generate a key for accessing the previous successful build's internal Carthage build files.
+ */
+
+const generateBuildFilesCacheKey = async () => {
+    const {stdout} = await execa('xcodebuild', ['-version']);
+    const hash = crypto.createHash('sha1').update(stdout).digest('hex');
+    return `devbotsxyz:carthage-build-files:${process.env.GITHUB_REPOSITORY}:${hash}`;
 };
 
 
-const carthageBootstrap = async ({platform, noUseBinaries, verbose, gitHubToken}) => {
+/**
+ * Restore the internal Carthage build files cache.
+ */
+
+const restoreBuildFilesCache = async () => {
+    const key = await generateBuildFilesCacheKey();
+    core.info(`Restoring internal build files cache with key ${key}`);
+    const result = await cache.restoreCache([`${process.env.HOME}/Library/Caches/org.carthage.CarthageKit`, `${process.env.HOME}/Library/Caches/carthage`], key);
+    core.info(`Result of cache.restore is ${result !== undefined}`);
+    return result !== undefined;
+};
+
+
+/*
+ * Save the internal Carthage build files cache.
+ */
+
+const saveBuildFilesCache = async () => {
+    try {
+        const key = await generateBuildFilesCacheKey();
+        core.info(`Saving internal build files cache with key ${key}`);
+        await cache.saveCache([`${process.env.HOME}/Library/Caches/org.carthage.CarthageKit`, `${process.env.HOME}/Library/Caches/carthage`], key + process.env.GITHUB_RUN_ID, key);
+        return true;
+    } catch (error) {
+        core.error(`Carthage could not cache internal build files: ${error.message}`);
+        return false;
+    }
+};
+
+const parseOptions = () => {
+    const options = {
+        verbose: core.getInput("verbose") === "true",
+        noUseBinaries: core.getInput("no-use-binaries") === "true",
+        useNetRC: core.getInput("use-netrc") === "true",
+        skippingSchemes: core.getInput("skipping-schemes"),
+        xcconfig: core.getInput("xcconfig"),
+        configuration: core.getInput("configuration"),
+        platform: core.getInput("platform"),
+        cache: core.getInput("cache") === "true", // TODO cache=true should be a default
+        cacheBuildFiles: core.getInput("cache-build-files") === "true",
+        cacheKey: core.getInput("cache-key"),
+        gitHubToken: core.getInput("github-token", {required: true}), // Not required when using SSH?
+    };
+    return options;
+};
+
+
+const carthageBootstrap = async ({platform, noUseBinaries, verbose, gitHubToken, useNetRC, xcconfig, skippingSchemes, cacheBuildFiles, configuration}) => {
     let options = [];
     if (platform !== "") {
         options = [...options, "--platform", platform];
     }
-    if (verbose === "true") {
+    if (verbose) {
         options = [...options, "--verbose"];
     }
-    if (noUseBinaries === "true") {
+    if (noUseBinaries) {
         options = [...options, "--no-use-binaries"];
+    }
+    if (useNetRC) {
+        options = [...options, "--use-netrc"];
+        fs.writeFileSync(`${process.env.HOME}/.netrc`, `machine github.com login ${process.env.GITHUB_ACTOR} password ${gitHubToken}`);
+    }
+    if (xcconfig !== "") {
+        process.env["XCODE_XCCONFIG_FILE"] = xcconfig;
+    }
+    if (cacheBuildFiles) {
+        options = [...options, "--cache-builds"];
+    }
+    if (configuration !== "") {
+        options = [...options, "--configuration", configuration];
+    }
+
+    core.info(`options ${options.join(" ")}`)
+
+    // this check should be performed after all other options are added
+    if (skippingSchemes !== "") {
+        const carthage = execa("carthage", ["bootstrap", ...options, "--no-build"],
+                               {reject: false, env: {"NSUnbufferedIO": "YES",
+                                                     "GITHUB_ACCESS_TOKEN": gitHubToken}});
+
+        var output = '';
+        carthage.stdout.on('data', data => { output += data.toString() });
+        carthage.stderr.on('data', data => { output += data.toString() });
+
+        let {exitCode} = await carthage;
+
+        if (exitCode != 0) {
+            core.info(output);
+            throw Error(`Carthage bootstrap failed with exit code ${exitCode}`);
+        }
+
+        skippingSchemes.split(",").map(function (x) { return { target: x.split(":")[0], scheme: x.split(":")[1] } }).map(x => {
+            const path = `Carthage/Checkouts/${x.target}/${x.target}.xcodeproj/xcshareddata/xcschemes/${x.scheme}.xcscheme`;
+            try {
+                core.info(`Removing the scheme at ${path}`);
+                fs.unlinkSync(path);
+            } catch(err) {
+                throw Error(`Unable to find matching scheme at ${path}`);
+            }
+        })
     }
 
     const carthage = execa("carthage", ["bootstrap", ...options],
@@ -137,14 +228,14 @@ const main = async () => {
     }
 
     try {
-        const configuration = parseConfiguration();
+        const options = parseOptions();
 
         // If caching is enabled and we have a cached Carthage
         // available for our dependencies then we just restore and be
         // done with it.
 
-        if (configuration.cache) {
-            const restored = await restoreCache(configuration);
+        if (options.cache) {
+            const restored = await restoreCache(options);
             if (restored) {
                 core.info("Restored Carthage from cache");
                 // TODO For clarity print all the dependencies and their versions
@@ -152,16 +243,23 @@ const main = async () => {
             }
         }
 
+        if (options.cacheBuildFiles) {
+            const restored = await restoreBuildFilesCache();
+            if (restored) {
+                core.info("Restored Carthage build files cache");
+            }
+        }
+
         // Either no caching was requested or we had not prior build
         // cached, so we run carthage bootstrap as usual.
 
-        await carthageBootstrap(configuration);
+        await carthageBootstrap(options);
 
         // We just built Carthage from scratch. So if caching was
         // requested then let the post script know that it needs to be
         // saved.
 
-        if (configuration.cache) {
+        if (options.cache) {
             core.saveState("needsSaveCache", "true");
         }
     } catch (error) {
@@ -172,16 +270,24 @@ const main = async () => {
 
 const post = async () => {
     try {
-        const configuration = parseConfiguration();
+        const options = parseOptions();
 
         // If caching is enabled and we built carthage from scratch,
         // then this is where we cache the Carthage folder.
 
-        if (configuration.cache && core.getState("needsSaveCache") === "true") {
+        if (options.cache && core.getState("needsSaveCache") === "true") {
             core.info("Going to save cache");
-            const saved = await saveCache(configuration);
+            const saved = await saveCache(options);
             if (saved) {
                 core.info("Saved cache");
+            }
+        }
+
+        if (options.cacheBuildFiles && core.getState("needsSaveCache") === "true") {
+            core.info("Going to save build files cache");
+            const saved = await saveBuildFilesCache(options);
+            if (saved) {
+                core.info("Saved build files cache");
             }
         }
     } catch (error) {
